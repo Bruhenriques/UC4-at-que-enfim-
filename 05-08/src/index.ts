505af6ee1e9326d8a19fa03e2169d8adcc97b2c5
Bruno Henrique de Souza <br.souzah1@gmail.com>
2025-08-11 19:53:17 -0300

    commit inicial

--- 05-08/src/index.ts
+++ 05-08/src/index.ts
@@ -122,3 +122,13 @@ while (i < pokemons.length) {
     console.log(`Capturando ${pokemon}`);
     i++; 
 }
+
+let passos = 0;
+const passosParaMordor = 5;
+
+do {
+  passos++;
+  console.log(`Dando passo ${passos}`);
+} while (passos < passosParaMordor);
+
+console.log("Chegamos em Mordor!");
